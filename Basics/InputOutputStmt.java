b7a5d5a441c0550b3669b5d6cc45a4efff76d901
amtulmahvish <amtulmahvishengr@gmail.com>
2025-10-20 16:16:57 +0530

    input output statements

---
+++ Basics/InputOutputStmt.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Solution {
+    
+    public void printNumber(Scanner sc) {
+        
+        int number = sc.nextInt();
+        System.out.print(number);
+    }    
+}
