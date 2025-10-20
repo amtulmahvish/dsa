bd88075f15625511eb44a2a0c79effa6204344c2
amtulmahvish <amtulmahvishengr@gmail.com>
2025-10-20 16:11:16 +0530

    comment changed

--- Basics/AddDigits.java
+++ Basics/AddDigits.java
@@ -1,7 +1,7 @@
 class AddDigits {
     public int addDigits(int num) {
         if(num == 0) return 0;
-        else if(num % 9 == 0) return 9; // divisibility rule of 9
+        else if(num % 9 == 0) return 9; // divisibility rule of 9 test commit
         else return num % 9;
     }
 }
