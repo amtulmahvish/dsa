a22cd59c5b315fb769306f78fadb1db49ccecf64
amtulmahvish <amtulmahvishengr@gmail.com>
2025-10-24 13:09:54 +0530

    pattern 10

---
+++ Patterns/Pattern10.java
@@ -0,0 +1,18 @@
+class Solution {
+ 
+    public static void pattern10(int n) {
+       
+        for (int i = 1; i <= 2 * n - 1; i++) {
+
+            int stars = i;
+
+            if (i > n) stars = 2 * n - i;
+
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/Pattern11.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void pattern11(int n) {
+        int start=1;
+        for(int i=0; i<n; i++){
+            
+            if(i %2==0) start = 1;
+            else start =0;
+            
+            for(int j=0;j<=i;j++){
+                System.out.print(start+" ");
+                start=1-start;
+            }
+            System.out.println();
+        }
+        
+    }
+}
